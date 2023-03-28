0ef789e49c3ba594b0f4c0149b497418e88f5b82
Gianluca Guarini <gianluca.guarini@axa.ch>
2023-03-28 11:47:09 +0200

    chore: Fix small details and get ready to merge it

--- formats/tailwind/index.js
+++ formats/tailwind/index.js
@@ -1,7 +1,7 @@
 const { getFontFamily } = require('./get-font-family');
 const { getColors } = require('./get-colors');
 const { getFontSize } = require('./get-font-size');
-const { getDropShadows } = require('./get-drop-shadow');
+const { getBoxShadow } = require('./get-box-shadow');
 const { getBorderRadius } = require('./get-border-radius');
 const { getScreens } = require('./get-screens');
 const { getSpacing } = require('./get-spacing');
@@ -14,13 +14,13 @@ module.exports.tailwindFormatter = ({ dictionary }) =>
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     theme: {
+        fontFamily:  ${formatTokens(getFontFamily(dictionary))},
+        fontSize:  ${formatTokens(getFontSize(dictionary))},
         extend: {
            screens: ${formatTokens(getScreens(dictionary))},
            spacing: ${formatTokens(getSpacing(dictionary))},
            colors: ${formatTokens(getColors(dictionary))},
-           fontFamily:  ${formatTokens(getFontFamily(dictionary))},
-           fontSize:  ${formatTokens(getFontSize(dictionary))},
-           dropShadow: ${formatTokens(getDropShadows(dictionary))},
+           boxShadow: ${formatTokens(getBoxShadow(dictionary))},
            borderRadius: ${formatTokens(getBorderRadius(dictionary))},
            transitionTimingFunction: ${formatTokens(getTransitionTimingFunction(dictionary))},
         }
