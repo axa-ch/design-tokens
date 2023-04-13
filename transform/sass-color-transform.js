5af32b1ef67ed63afb7c52e32922266df6240f4d
Gianluca Guarini <gianluca.guarini@axa.ch>
2023-04-13 08:47:37 +0200

    chore: Add dark and light color shades

--- transform/sass-color-transform.js
+++ transform/sass-color-transform.js
@@ -1,8 +1,9 @@
 const sass = require('sass');
 
 // assuming that the input is a 7 chars string length
-const hexToRgb = (hex) => {
-  if (hex.length < 7) throw new Error('Shorthand hex colors are not supported');
+const hexToRgb = (hex, name) => {
+  if (!hex) throw new Error(`The value for ${name} is undefined`);
+  if (hex.length < 7) throw new Error(`The value provided for ${name} doesn't seem to be in hex format`);
   return {
     red: parseInt(hex.slice(1, 3), 16),
     green: parseInt(hex.slice(3, 5), 16),
@@ -15,10 +16,21 @@ module.exports.sassColorTransform = {
   transitive: true,
   transformer: (token) => {
     if (!token.params) throw new Error('Please provide the params key needed for the sass.color.change method');
-    const rgb = hexToRgb(token.value);
+    const rgb = hexToRgb(token.value, token.name);
     const color = new sass.SassColor({ ...rgb });
+    // use the values modifying the initial color properties for example:
+    // given token.params.alpha - 0.2 -> we reduce the opacity of the 20%
+    // color.alpha() + token.params.alpha
+    const relativeValues = Object.entries(token.params).reduce(
+      (acc, [key, value]) => ({ ...acc, [key]: color[key] + value }),
+      {},
+    );
 
-    // see https://sass-lang.com/documentation/js-api/classes/SassColor#change for the available params
-    return color.change(token.params).toString();
+    try {
+      // see https://sass-lang.com/documentation/js-api/classes/SassColor#change for the available params
+      return color.change(relativeValues).toString();
+    } catch (error) {
+      throw new Error(`Unable to change the color ${token.name}`, { cause: error });
+    }
   },
 };
