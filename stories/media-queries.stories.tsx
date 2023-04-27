34b47e7c3960324a39e74a134c6508ba8e497db6
Gianluca Guarini <gianluca.guarini@axa.ch>
2023-04-27 09:06:31 +0200

    fix: CSS comments

--- stories/media-queries.stories.tsx
+++ stories/media-queries.stories.tsx
@@ -129,13 +129,13 @@ export const MediaQueries: StoryFn = () => (
 @custom-media --mq-base-landscape (orientation: landscape);
 
 /* Responsive design media queries */
-// No media query necessary for xs breakpoint as it's effectively '@media (max-width: 0) { ... }'
+/*  No media query necessary for xs breakpoint as it's effectively '@media (max-width: 0) { ... }' */
 @custom-media --mq-respond-down-sm (max-width: 575.98px);
 @custom-media --mq-respond-down-md (max-width: 767.98px);
 @custom-media --mq-respond-down-lg (max-width: 991.98px);
 @custom-media --mq-respond-down-xl (max-width: 1199.98px);
 @custom-media --mq-respond-down-xxl (max-width: 1439.98px);
-// No media query necessary for xs breakpoint as it's effectively '@media (min-width: 0) { ... }'
+/* No media query necessary for xs breakpoint as it's effectively '@media (min-width: 0) { ... }' */
 @custom-media --mq-respond-up-sm (min-width: 576px);
 @custom-media --mq-respond-up-md (min-width: 768px);
 @custom-media --mq-respond-up-lg (min-width: 992px);
