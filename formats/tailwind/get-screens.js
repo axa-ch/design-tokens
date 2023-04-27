59170e5ada424005bdbd28d228300da49e467d03
Dominik Forster <domi@imod.ch>
2023-04-27 09:02:02 +0200

    feat: update breakpoint naming (#1)
    * feat: update breakpoint naming
    
    update breakpoint naming to match common breakpoint names and map tailwind screen xxl to 2xl
    
    * docs: update media query docs
    
    update media query docs to align with the latest naming changes
    
    * fix: add xs base breakpoint for consistency

--- formats/tailwind/get-screens.js
+++ formats/tailwind/get-screens.js
@@ -4,12 +4,12 @@ const getBreakpointTokens = filterTokenByPathName('breakpoints');
 const mapBreakpoints = (tokens) =>
   tokens.reduce(
     (acc, token) =>
-      // filter out the breakpoints max boundaries
+      // filter out the breakpoints max boundaries and map the breakpoint names to match tailwind defaults
       token.name.includes('max')
         ? acc
         : {
             ...acc,
-            [token.name.replace(`breakpoints-base-`, '')]: `${token.value}px`,
+            [token.name.replace(`breakpoints-base-`, '').replace('xxl', '2xl')]: `${token.value}px`,
           },
     {},
   );
