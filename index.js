cae8e3a602210ae0a36954b244f516462185cbb3
Gianluca Guarini <gianluca.guarini@gmail.com>
2023-10-24 10:49:28 +0200

    fix: Linting errors

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-export * from './build/js/mq';
-export * from './build/js/breakpoints';
-export * from './build/js/animation';
-export * from './build/js/color';
-export * from './build/js/radius';
-export * from './build/js/shadow';
-export * from './build/js/spacing';
-export * from './build/js/typography';
-
 // Export also the bundled css
 // import './build/css/mq.css'; // mq css will be bundled when all browsers will support @custom-media directives
 import './build/css/shadow.css';
@@ -16,3 +7,12 @@ import './build/css/breakpoints.css';
 import './build/css/radius.css';
 import './build/css/spacing.css';
 import './build/css/typography.css';
+
+export * from './build/js/mq';
+export * from './build/js/breakpoints';
+export * from './build/js/animation';
+export * from './build/js/color';
+export * from './build/js/radius';
+export * from './build/js/shadow';
+export * from './build/js/spacing';
+export * from './build/js/typography';
