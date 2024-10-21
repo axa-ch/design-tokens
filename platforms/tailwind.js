88508670153629354210c157c18e615684cb0aae
Gianluca Guarini <gianluca.guarini@gmail.com>
2024-10-21 14:13:41 +0200

    fix: unit test

--- platforms/tailwind.js
+++ platforms/tailwind.js
@@ -1,9 +1,28 @@
 import { MEDIA_QUERY_TOKEN_NAME, OUTPUT_BASE_DIR, SHARED_TRANSFORM_LIST } from './constants.js';
-import styleDictionary from 'style-dictionary';
 
 export const tailwind = {
   buildPath: `${OUTPUT_BASE_DIR}/tailwind/`,
-  transforms: ['shadow/css', 'cubicBezier/css', 'size/px', 'dimension/pixelToRem', ...SHARED_TRANSFORM_LIST],
+  transforms: [
+    'shadow/css',
+    'cubicBezier/css',
+    'size/px',
+    'dimension/pixelToRem',
+    'attribute/cti',
+    'name/kebab',
+    'time/seconds',
+    'html/icon',
+    'size/rem',
+    'color/css',
+    'asset/url',
+    'fontFamily/css',
+    'cubicBezier/css',
+    // object-value tokens
+    'strokeStyle/css/shorthand',
+    'border/css/shorthand',
+    'transition/css/shorthand',
+    'shadow/css/shorthand',
+    ...SHARED_TRANSFORM_LIST,
+  ],
   files: [
     {
       destination: 'tailwind.config.js',
