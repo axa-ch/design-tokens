3ad74ce45f35e6e250698512370fc699cf8a9baa
Markus Szarvas <markus.szarvas@axa.ch>
2025-04-28 10:01:51 +0200

    Refactors font size mapping for improved maintenance

--- formats/tailwind-theme-css/get-font-sizes.ts
+++ formats/tailwind-theme-css/get-font-sizes.ts
@@ -1,32 +1,16 @@
 export const replaceStyleDictionaryFontSizesWithTailwindFontSizes = (tokenName: string, itemNumber: string) => {
-  if (itemNumber === '1') {
-    return tokenName.replace('1', 'xs');
-  }
-  if (itemNumber === '2') {
-    return tokenName.replace('2', 'sm');
-  }
-  if (itemNumber === '3') {
-    return tokenName.replace('3', 'base');
-  }
-  if (itemNumber === '4') {
-    return tokenName.replace('4', 'lg');
-  }
-  if (itemNumber === '5') {
-    return tokenName.replace('5', 'xl');
-  }
-  if (itemNumber === '6') {
-    return tokenName.replace('6', '2xl');
-  }
-  if (itemNumber === '7') {
-    return tokenName.replace('7', '3xl');
-  }
-  if (itemNumber === '8') {
-    return tokenName.replace('8', '4xl');
-  }
-  if (itemNumber === '9') {
-    return tokenName.replace('9', '5xl');
-  }
-  if (itemNumber === '10') {
-    return tokenName.replace('10', '6xl');
-  }
+  const sizeMapping: Record<string, string> = {
+    '1': 'xs',
+    '2': 'sm',
+    '3': 'base',
+    '4': 'lg',
+    '5': 'xl',
+    '6': '2xl',
+    '7': '3xl',
+    '8': '4xl',
+    '9': '5xl',
+    '10': '6xl',
+  };
+
+  return sizeMapping[itemNumber] ? tokenName.replace(itemNumber, sizeMapping[itemNumber]) : tokenName;
 };
