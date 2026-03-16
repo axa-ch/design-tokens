7880c564d28f9987419ef501527cbc0fc0330562
Gianluca Guarini <gianluca.guarini@gmail.com>
2026-03-16 11:18:37 +0100

    chore: Use named export to identify the transform ids

--- platforms/json.ts
+++ platforms/json.ts
@@ -1,9 +1,16 @@
+import { transforms } from 'style-dictionary/enums';
 import { SHARED_TRANSFORM_LIST, MEDIA_QUERY_TOKEN_NAME } from './constants';
 import type { PlatformConfig } from 'style-dictionary/types';
 
 export const json: PlatformConfig = {
   buildPath: './',
-  transforms: ['attribute/cti', 'name/kebab', 'size/px', 'color/css', 'size/px', ...SHARED_TRANSFORM_LIST],
+  transforms: [
+    transforms.attributeCti,
+    transforms.nameKebab,
+    transforms.sizePx,
+    transforms.colorCss,
+    ...SHARED_TRANSFORM_LIST,
+  ],
   files: [
     {
       destination: 'tokens.json',
--- platforms/tailwind.ts
+++ platforms/tailwind.ts
@@ -1,27 +1,27 @@
+import { transforms } from 'style-dictionary/enums';
 import type { PlatformConfig } from 'style-dictionary/types';
-import { MEDIA_QUERY_TOKEN_NAME, OUTPUT_BASE_DIR, SHARED_TRANSFORM_LIST } from './constants';
+import { MEDIA_QUERY_TOKEN_NAME, OUTPUT_BASE_DIR, shadowCssTransform, SHARED_TRANSFORM_LIST } from './constants';
 
 export const tailwind: PlatformConfig = {
   buildPath: `${OUTPUT_BASE_DIR}/tailwind/`,
   transforms: [
-    'shadow/css',
-    'cubicBezier/css',
-    'size/px',
-    'size/pxToRem',
-    'attribute/cti',
-    'name/kebab',
-    'time/seconds',
-    'html/icon',
-    'size/rem',
-    'color/css',
-    'asset/url',
-    'fontFamily/css',
-    'cubicBezier/css',
+    shadowCssTransform,
+    transforms.cubicBezierCss,
+    transforms.sizePx,
+    transforms.sizePxToRem,
+    transforms.attributeCti,
+    transforms.nameKebab,
+    transforms.timeSeconds,
+    transforms.htmlIcon,
+    transforms.sizeRem,
+    transforms.colorCss,
+    transforms.assetUrl,
+    transforms.fontFamilyCss,
     // object-value tokens
-    'strokeStyle/css/shorthand',
-    'border/css/shorthand',
-    'transition/css/shorthand',
-    'shadow/css/shorthand',
+    transforms.strokeStyleCssShorthand,
+    transforms.borderCssShorthand,
+    transforms.transitionCssShorthand,
+    transforms.shadowCssShorthand,
     ...SHARED_TRANSFORM_LIST,
   ],
   files: [
