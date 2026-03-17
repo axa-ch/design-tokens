677eb9fec86574aac5c6f3235d3b036777137bf5
Gianluca Guarini <gianluca.guarini@gmail.com>
2026-03-17 12:52:09 +0100

    chore: Update the color cards adding the deprecated style

--- stories/components/color-card/color-card.tsx
+++ stories/components/color-card/color-card.tsx
@@ -1,16 +1,18 @@
 import { type CSSProperties, type FC, useMemo } from 'react';
+import clsx from 'clsx';
 import { startCase } from 'lodash';
 import classes from './color-card.module.scss';
 
 export type ColorCardProps = {
   name: string;
   value: string;
+  isDeprecated?: boolean;
 };
 
 /**
  * Primary UI component for user interaction
  */
-export const ColorCard: FC<ColorCardProps> = ({ name, value }) => {
+export const ColorCard: FC<ColorCardProps> = ({ name, value, isDeprecated }) => {
   // format the color name making it human-readable
   const formattedName = useMemo(
     () => startCase(name.replace(/color-(misc|ui-design|base|status)|color-/, '')).replace('Axa', 'AXA'),
@@ -18,7 +20,7 @@ export const ColorCard: FC<ColorCardProps> = ({ name, value }) => {
   );
   return (
     <div
-      className={classes.container}
+      className={clsx(classes.container, { [classes.deprecated]: isDeprecated })}
       style={
         {
           '--bg': `var(--${name})`,
@@ -29,6 +31,7 @@ export const ColorCard: FC<ColorCardProps> = ({ name, value }) => {
       <h1 className={classes.name}>{formattedName}</h1>
       <h2 className={classes.value}>{value}</h2>
       <h3 className={classes.variableName}>{name}</h3>
+      {isDeprecated ? <h3 className={classes.info}>Deprecated</h3> : null}
     </div>
   );
 };
